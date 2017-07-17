aec0930506225c9f6d1cb1032162c7737a7bb55c
Diman Todorov <diman.todorov@outlook.com>
2017-07-16 17:37:42 -0700

    moved settings button to top right

--- mobile/src/main/java/com/dtodorov/zerobeat/MainActivity.java
+++ mobile/src/main/java/com/dtodorov/zerobeat/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -41,6 +43,7 @@ public class MainActivity extends AppCompatActivity
     private MainController mainController;
     private IEventDispatcher eventDispatcher;
     private Configuration configuration;
+    private FloatingMusicActionButton buttonMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -84,26 +87,13 @@ public class MainActivity extends AppCompatActivity
                 eventDispatcher,
                 school);
 
-        final FloatingMusicActionButton buttonMusic = (FloatingMusicActionButton) findViewById(R.id.button_music);
+        buttonMusic = (FloatingMusicActionButton) findViewById(R.id.button_music);
         buttonMusic.setOnMusicFabClickListener(new FloatingMusicActionButton.OnMusicFabClickListener()
         {
             @Override
             public void onClick(@NotNull View view)
             {
                 mainController.fire(MainController.Trigger.Play, null);
-            }
-        });
-
-        ImageButton buttonSettings = (ImageButton) findViewById(R.id.button_settings);
-        buttonSettings.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                mainController.fire(MainController.Trigger.Stop, null);
-                buttonMusic.changeMode(FloatingMusicActionButton.Mode.PLAY_TO_STOP);
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivityForResult(intent, MainActivity.REQUEST_CODE_PREFERENCES);
             }
         });
     }
@@ -125,6 +115,27 @@ public class MainActivity extends AppCompatActivity
         configuration.setChannels(1);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                mainController.fire(MainController.Trigger.Stop, null);
+                buttonMusic.changeMode(FloatingMusicActionButton.Mode.PLAY_TO_STOP);
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivityForResult(intent, MainActivity.REQUEST_CODE_PREFERENCES);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
     @Override
     public void onActivityResult (int requestCode, int resultCode, Intent data)
     {
