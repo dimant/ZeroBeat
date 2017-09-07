7289f489e0e180122dfa3f19d2413fac0d561a1c
Diman Todorov <diman.todorov@outlook.com>
2017-09-06 21:47:46 -0700

    changed cw lessons

--- mobile/src/main/java/com/dtodorov/zerobeat/teacher/Lessons.java
+++ mobile/src/main/java/com/dtodorov/zerobeat/teacher/Lessons.java
@@ -15,13 +15,24 @@ public class Lessons
     {
         this.lessons = new ArrayList<>();
 
-        lessons.add("eishtmo");
-        lessons.add("awjndb");
-        lessons.add("uvgzkrpx");
-        lessons.add("fclqy");
-        lessons.add("12345");
-        lessons.add("67890");
-        lessons.add(".,?/");
+//        lessons.add("eishtmo");
+//        lessons.add("awjndb");
+//        lessons.add("uvgzkrpx");
+//        lessons.add("fclqy");
+//        lessons.add("12345");
+//        lessons.add("67890");
+//        lessons.add(".,?/");
+        lessons.add("tean");
+        lessons.add("teanois14");
+        lessons.add("ois14rhd25");
+        lessons.add("14rhd25luc");
+        lessons.add("25lucmw36?");
+        lessons.add("mw36?fy");
+        lessons.add("36?fypg79/");
+        lessons.add("pg79/bv");
+        lessons.add("bvkj80");
+        lessons.add("kj80xqz");
+
         Collections.unmodifiableList(lessons);
     }
 
